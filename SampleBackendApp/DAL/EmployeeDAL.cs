b63b9240b0189e4353aea36289ea894ab8dce60f
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-03 19:52:54 +0700

    menambahkan select employee

--- SampleBackendApp/DAL/EmployeeDAL.cs
+++ SampleBackendApp/DAL/EmployeeDAL.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Web.Configuration;
+using SampleBackendApp.Models;
 
 namespace SampleBackendApp.DAL
 {
@@ -10,7 +12,38 @@ namespace SampleBackendApp.DAL
     {
         private string GetConn()
         {
-            return "";
+            return WebConfigurationManager.ConnectionStrings["MyConnectionString"]
+                .ConnectionString;
         }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            using(SqlConnection conn = new SqlConnection(GetConn()))
+            {
+                string strSql = @"select * from Employees order by EmpName asc";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        lstEmployee.Add(new Employee
+                        {
+                            EmpId = Convert.ToInt32(dr["EmpId"]),
+                            EmpName = dr["EmpName"].ToString(),
+                            Department = dr["Department"].ToString(),
+                            Designation = dr["Designation"].ToString(),
+                            Qualification = dr["Qualification"].ToString()
+                        });
+                    }
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return lstEmployee;
+        } 
     }
 }
